5d93cfa76352c7f35c26bc9b2e95f8cb535f05ff
Tabassum Tabassum <ttabassu@qti.qualcomm.com>
2025-09-18 12:27:40 +0530

    qssi_au_64: Enable GAS packages
    Enable GAS packages
    
    Change-Id: I528e9d02a80e9dade4ae6761ce5800499d678d1c
    Signed-off-by: Tabassum Tabassum <ttabassu@qti.qualcomm.com>
    (cherry picked from commit 02f249e9765264f736f789adcaa576bcc69b7c7d)

--- qssi_au_64.mk
+++ qssi_au_64.mk
@@ -91,7 +91,7 @@ TARGET_USES_QSSI := true
 
 TARGET_USES_NEW_ION := true
 
-TARGET_USES_GAS := false
+TARGET_USES_GAS := true
 
 ifeq ($(strip $(TARGET_BUILD_VARIANT)),user)
 ifeq ($(TARGET_USES_GAS),true)
