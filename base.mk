b4ec71265097a958bb7b4653664f1b19f1c452b7
Rohit Nehra <rnehra@qti.qualcomm.com>
2026-06-09 12:46:53 +0530

    qssi_au_64 : removing propeties from system.prop
    Move persist.bluetooth.enablenewavrcp, config.disable_rtt, and
    persist.debug.wfd.enable from system.prop to PRODUCT_SYSTEM_PROPERTIES
    
    Change-Id: Ia706e49f2e7e3e4b94556e7110df80065f13bda8
    Signed-off-by: Rohit Nehra <rnehra@qti.qualcomm.com>

--- base.mk
+++ base.mk
@@ -861,11 +861,6 @@ ifneq ($(BOARD_AVB_ENABLE), true)
    $(call inherit-product, build/target/product/verity.mk)
 endif
 
-ifeq ($(TARGET_BUILD_VARIANT),user)
-PRODUCT_DEFAULT_PROPERTY_OVERRIDES+= \
-    ro.adb.secure=1
-endif
-
 # OEM Unlock reporting
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
     ro.oem_unlock_supported=1
